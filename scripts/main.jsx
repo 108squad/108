066a51652715088971dc4a0f0d726213441ffd0d
Andrey <andbogc@gmail.com>
2018-03-10 21:21:10 +0300

    Login page and fixes

---
+++ scripts/main.jsx
@@ -0,0 +1,45 @@
+class Signin extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.state = {
+            email:'',
+            password:''
+        };
+    }
+
+    signIn(){
+        alert('Email is ' + this.state.email + '\nPass is ' + this.state.password);
+    }
+
+    handleEmailChange(e) {
+        this.setState({email: e.target.value});
+    }
+
+    handlePasswordChange(e) {
+        this.setState({password: e.target.value});
+    }
+
+    render() {
+        return (
+            <form className="form-signin">
+                <h2 className="form-signin-heading"> Please sign in </h2>
+                <label for="inputEmail" className="sr-only"> Email address</label>
+                <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                <label for="inputPassword" className="sr-only"> Password</label>
+                <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+                <button className="btn btn-lg btn-primary btn-block" onClick={this.signIn} type="button"> Signin</button>
+            </form>
+        )
+    }
+
+
+
+}
+
+ReactDOM.render(
+        <Signin />,
+    document.getElementById('app'));
